1362ae05c9418f586f3ec29745d389bb778fd28b
aibyn322 <151915115+aibyn322@users.noreply.github.com>
2025-04-27 00:43:25 +0500

    Update views.py

--- hospital/views.py
+++ hospital/views.py
@@ -68,7 +68,7 @@ def doctor_signup_view(request):
             user.save()
             doctor=doctorForm.save(commit=False)
             doctor.user=user
-            doctor=doctor.save()
+            doctor.save()
             my_doctor_group = Group.objects.get_or_create(name='DOCTOR')
             my_doctor_group[0].user_set.add(user)
         return HttpResponseRedirect('doctorlogin')
@@ -89,7 +89,7 @@ def patient_signup_view(request):
             patient=patientForm.save(commit=False)
             patient.user=user
             patient.assignedDoctorId=request.POST.get('assignedDoctorId')
-            patient=patient.save()
+            patient.save()
             my_patient_group = Group.objects.get_or_create(name='PATIENT')
             my_patient_group[0].user_set.add(user)
         return HttpResponseRedirect('patientlogin')
